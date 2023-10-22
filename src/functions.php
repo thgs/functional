258ce2690ae11dfba285f6302b16b04512d0ed49
thgs <theogpl57@gmail.com>
2023-10-22 02:48:55 +0100

    Add either function

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,9 @@
 
 namespace thgs\Functional;
 
+use thgs\Functional\Data\Either;
+use thgs\Functional\Data\Left;
+use thgs\Functional\Data\Right;
 use thgs\Functional\Typeclass\FunctorInstance as F;
 use thgs\Functional\Typeclass\ShowInstance;
 
@@ -24,4 +27,23 @@ function fmap(callable $f, F $g): F {
 function show(int|string|float|bool|ShowInstance $x): string
 {
     return (string) $x;
+}
+
+/**
+ * @template A
+ * @template B
+ * @template C
+ *
+ * @param callable(A): C $f
+ * @param callable(B): C $g
+ * @param Either $either
+ * @return C
+ */
+function either(callable $f, callable $g, Either $either)
+{
+    $value = $either->getValue();
+    return match (\true) {
+        $value instanceof Left => $f($value->getValue()),
+        $value instanceof Right => $g($value->getValue())
+    };
 }
