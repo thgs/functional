172946eb4925e2b20d8e93dfb383f0f2c1203b6a
Theo Gotsopoulos <theogpl57@gmail.com>
2025-03-10 09:35:41 +0000

    Add a dubious unwrap in Maybe
    There must be a better way

--- src/Data/Maybe.php
+++ src/Data/Maybe.php
@@ -53,6 +53,14 @@ class Maybe implements
     }
 
     /**
+     * @return A1|null
+     */
+    public function unwrap()
+    {
+        return $this->x->getValue();
+    }
+
+    /**
      * instance Functor Maybe where
      *      fmap f (Just x) = Just (f x)
      *      fmap f Nothing = Nothing
