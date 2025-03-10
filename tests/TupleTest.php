305697091aa01e879c8d78968f2d6e1e8a93504e
Theo Gotsopoulos <theogpl57@gmail.com>
2025-03-10 11:54:52 +0000

    Add tests for Tuple

---
+++ tests/TupleTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use thgs\Functional\Data\Tuple;
+
+use function thgs\Functional\partial;
+use function thgs\Functional\t;
+
+class TupleTest extends TestCase
+{
+    public function testCanCreateTuple(): void
+    {
+        $tuple = Tuple::new(123, "hello world");
+        $this->assertTupleIs(123, "hello world", $tuple);
+    }
+
+    public function testCanSwapTupleValues(): void
+    {
+        $tuple = Tuple::new(123, "hello world")->swap();
+        $this->assertTupleIs("hello world", 123, $tuple);
+    }
+
+    public function testCanCreateADupe(): void
+    {
+        $dupe = Tuple::dupe(123);
+        $this->assertTupleIs(123, 123, $dupe);
+    }
+
+    public function testCanApplyFunctionToBoth(): void
+    {
+        $dupe = Tuple::dupe(125);
+        $both = Tuple::both(fn ($x) => $x * 2, $dupe);
+        $this->assertTupleIs(250, 250, $both);
+    }
+
+    public function testCanCurry(): void
+    {
+        $result = Tuple::curry(
+            fn (Tuple $t) => [(string) $t->fst() => strtoupper($t->snd())],
+            12,
+            "twelve");
+
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('12', $result);
+        $this->assertEquals('TWELVE', $result['12']);
+    }
+
+    public function testCanCurryWithPartial(): void
+    {
+        $partialCurry = partial (Tuple::curry(...), fn (Tuple $t) => [(string) $t->fst() => strtoupper($t->snd())]);
+        $result = $partialCurry(12, "twelve");
+        // todo: the above works but we cannot give the arguments one by one, so partial seems to have an issue.
+        
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('12', $result);
+        $this->assertEquals('TWELVE', $result['12']);
+    }
+
+    public function testCanUncurry(): void
+    {
+        $f = fn ($a, $b) => $a * $b;
+
+        // todo: is this implemented only as partial/curried ?
+        $partialUncurry = Tuple::uncurry($f);
+
+        // Now calling it by passing a tuple
+        $result = $partialUncurry(t(10, 4));
+
+        $this->assertEquals(40, $result);
+    }
+
+    // todo: add equals
+
+    /**
+     * @template A
+     * @template B
+     * @param A $expectedA
+     * @param B $expectedB
+     * @param Tuple<A,B> $tuple
+     */
+    private function assertTupleIs(mixed $expectedA, mixed $expectedB, Tuple $tuple): void
+    {
+        $this->assertEquals($expectedA, $tuple->fst());
+        $this->assertEquals($expectedB, $tuple->snd());
+    }
+}
