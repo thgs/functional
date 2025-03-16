5411bc3ba5ec9b8a3578e0a293c3dab3429a14f3
Theo Gotsopoulos <theogpl57@gmail.com>
2025-03-16 13:04:03 +0000

    Remove comments from example

--- examples/io/files.php
+++ examples/io/files.php
@@ -9,18 +9,9 @@ use function thgs\Functional\readFile;
 use function thgs\Functional\show;
 use function thgs\Functional\writeFile;
 
-$ioAction = dn(
-    // IO<void>
+dn(
     writeFile("test.txt", show (123 * 123)),
-
-    // IO<void>    therefore will use (>>)
     appendFile("test.txt", "Hello!\n"),
-
-    // IO<string>  therefore will use (>>)
     readFile("test.txt"),
-
-    // callable    therefore will use (>>=)
     putStrLn(...)
-);
-
-$ioAction();
+)();
