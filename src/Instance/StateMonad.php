c2523606dc9338b9751ed4c90443e9652a2bfc84
Theo Gotsopoulos <theogpl57@gmail.com>
2025-03-28 01:20:24 +0000

    Add draft StateMonad

---
+++ src/Instance/StateMonad.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace thgs\Functional\Instance;
+
+use Closure;
+use thgs\Functional\Data\Tuple;
+use thgs\Functional\Typeclass\MonadInstance;
+
+use function thgs\Functional\partial;
+use function thgs\Functional\reflectNoOfArguments;
+use function thgs\Functional\t;
+
+/**
+ * @template S
+ * @template A
+ *
+ * newtype State s a = State (s -> (a, s))
+ * newtype State s a = State { runState :: s -> (a, s) }
+ *
+ * @implements MonadInstance<StateMonad<S,A>>
+ */
+class StateMonad implements
+    MonadInstance
+{
+    public function __construct(
+        /**
+         * @var \Closure(S):Tuple<A,S> $f
+         */
+        private \Closure $f
+    ) {
+    }
+
+    /**
+     * Only purpose of this, I guess, is to be able to write
+     *
+     * StateMonad::state(...)
+     *
+     * Or equally,
+     *
+     * partial (StateMonad::state(...))
+     *
+     * and pass this around.
+     *
+     * @template S1
+     * @template A1
+     * @param \Closure(S1):Tuple<A1,S1> $f
+     * @return StateMonad<S1,A1>
+     */
+    public static function state(\Closure $f): StateMonad
+    {
+        return new self($f);
+    }
+
+    /**
+     * runState :: State s a -> s -> (a, s)
+     * runState (State f) s = f s
+     *
+     * To help the first implementation for bind(), this is
+     * implemented as static.
+     * 
+     * @param StateMonad<S,A> $sa
+     * @param S $s
+     * @return Tuple<A,S>
+     *
+     * @todo this in 8.4 could be implemented as a getter?
+     */
+    public static function runState(StateMonad $sa, mixed $s): Tuple
+    {
+        return partial ($sa->f, $s);
+    }
+
+    /**
+     * @return StateMonad<S,S>
+     */
+    public function get(): StateMonad
+    {
+        return new self( fn ($s) => t($s, $s));
+    }
+
+    /**
+     * @todo: replace null from here
+     *
+     * @template S1
+     * @param S1 $sA
+     * @return StateMonad<S1, null>
+     */
+    public function put(mixed $sA): StateMonad
+    {
+        return new self( fn ($s) => t(null, $sA));
+    }
+
+    /**
+     * @template A0
+     * @template S0
+     * @param A0 $a
+     * @return StateMonad<S0,A0>
+     *
+     * return a = State (\s -> (a, s))
+     *
+     * return :: a -> State s a
+     * return x = state ( \ s -> (x, s) )
+     */
+    public static function inject($a): MonadInstance
+    {
+        return new self(
+            /** @var \Closure(S0):Tuple<A0,S0> */
+            fn ($s): Tuple => t($a, $s));
+    }
+
+    /**
+     * sa >>= k = State (\s -> let (a, s') = runState sa s 
+     *                         in runState (k a) s')
+     *
+     * (>>=) :: m a -> (a -> m b) -> m b
+     * Here it is with m being (State s).
+     * (>>=) :: State s a -> (a -> State s b) -> State s b
+     *
+     * @template B1
+     * @param \Closure(A):StateMonad<S,B1> $k
+     * @return StateMonad<S,B1>
+    */
+    public function bind(Closure $k): MonadInstance
+    {
+        $f = function ($s) use ($k) {
+            $az = static::runState ($this, $s);
+            [$a, $z] = [$az->fst(), $az->snd()]; // just for brevity, for now
+
+            $qState = partial ($k, $a);
+            return static::runState ($qState, $z);
+        };
+
+        return StateMonad::state($f);
+    }
+
+    /**
+     * @template S2
+     * @template A2
+     * @param StateMonad<S2,A2> $b
+     * @return StateMonad<S2,A2>
+     */
+    public function then(MonadInstance $b): MonadInstance
+    {
+        // todo: fix this
+        return $this->bind(fn ($s) => $b);
+    }
+}
---
+++ tests/StateMonadTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use thgs\Functional\Instance\StateMonad;
+use function thgs\Functional\dn;
+use function thgs\Functional\partial;
+use function thgs\Functional\t;
+
+class StateMonadTest extends TestCase
+{
+    public function testCanRunState(): void
+    {
+        /**
+         * @var StateMonad<bool,bool>
+         */
+        $state = StateMonad::state(function (bool $initialState) {
+            $newState = $output = true && $initialState;
+            return t($output, $newState);
+        });
+
+        $result = StateMonad::runState($state, true);
+        [$output, $newState] = [$result->fst(), $result->snd()];
+
+        $this->assertTrue($output);
+        $this->assertTrue($newState);
+
+        $result = StateMonad::runState($state, false);
+        [$output, $newState] = [$result->fst(), $result->snd()];
+
+        $this->assertFalse($output);
+        $this->assertFalse($newState);
+    }
+
+    public function testCanBind(): void
+    {
+        $andTrue = StateMonad::state(function (bool $initialState) {
+            $newState = $output = true && $initialState;
+            return t($output, $newState);
+        });
+        
+        $notTrue = StateMonad::state(function (bool $initialState) {
+            $newState = $output = !$initialState;
+            return t($output, $newState);
+        });
+
+        // todo: fix this bind usage.
+        $effectivelyXorFalse = $andTrue->bind(
+            fn ($x) => StateMonad::state( fn () => StateMonad::runState($notTrue, $x) )
+        );
+
+        $result = StateMonad::runState($effectivelyXorFalse, true);
+        [$output, $newState] = [$result->fst(), $result->snd()];
+        //var_dump(gettype($output), gettype($newState));
+
+        $this->assertFalse($output);
+        $this->assertFalse($newState);
+
+        $result = StateMonad::runState($effectivelyXorFalse, false);
+        [$output, $newState] = [$result->fst(), $result->snd()];
+        $this->assertTrue($output);
+        $this->assertTrue($newState);
+    }
+}
