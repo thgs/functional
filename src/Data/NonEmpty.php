d1ff040bb35c95dd1ea897d7dea50497edfa459a
Theo Gotsopoulos <theogpl57@gmail.com>
2025-03-31 20:00:52 +0100

    Add more generic NonEmpty

---
+++ src/Data/NonEmpty.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace thgs\Functional\Data;
+
+/**
+ * @template A of non-empty-string|non-empty-array|true|non-zero-int|object
+ * @todo there is no non-zero float
+ *
+ * This could be a typeclass and then NonEmptiness be defined for
+ * *any* type, when the non-emptiness is defined differently than the
+ * PHP's `empty()` behaviour.  For example, Maybe has Nothing which
+ * should be considered `empty`.
+ *
+ * @todo what about resources?
+ * @todo what about \Generators ?
+ * @todo what about Fibers?
+ */
+class NonEmpty
+{
+    public function __construct(
+        /** @var A $a */
+        private mixed $a
+    ) {
+        if (empty($this->a)) {
+            throw new \TypeError('Given value is empty.');
+        }
+
+        // todo: wrap around resources, \Generators, Fibers, whatnot
+        // is not following PHP's empty and is already available.
+        // todo: add the typeclass check here, if $a implements it.
+    }
+
+    /**
+     * @return A
+     */
+    public function nonEmptyValue(): mixed
+    {
+        return $this->a;
+    }
+}
+
