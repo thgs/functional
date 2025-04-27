3271033b096bfb8c55d73dfad69bdbc79ddab479
Theo Gotsopoulos <theogpl57@gmail.com>
2025-04-27 15:48:44 +0100

    Fix phpstan errors

--- src/Data/Constant.php
+++ src/Data/Constant.php
@@ -8,34 +8,57 @@ use thgs\Functional\Typeclass\FunctorInstance;
 use function thgs\Functional\c;
 
 /**
- * @template C
- * @template A
+ * @template C1
+ * @template A1
  *
- * @implements FunctorInstance<Constant<C,*>>
- * @implements BifunctorInstance<C,A>
+ * @implements FunctorInstance<Constant<C1,*>>
+ * @implements BifunctorInstance<C1,A1>
  */
 class Constant implements
     FunctorInstance,
     BifunctorInstance
 {
     public function __construct(
-        /** @var C */
+        /** @var C1 */
         private mixed $c
     ) {
     }
 
     /**
      * @template B1
-     * @param \Closure(A):B1 $f
-     * @return Constant<C,B1>
+     * @param \Closure(A1):B1 $f
+     * @return Constant<C1,B1>
      */
     public function fmap(\Closure $f): FunctorInstance
     {
+        /**
+         * Not sure how could this be fixed and carry the B1, apart
+         * from overriding it with a var annotation.  Instead, going
+         * to suppress the error and assume static analysis will
+         * resolve from the return annotation of the method.
+         *
+         * @phpstan-ignore return.type
+         */
         return new self($this->c);
     }
 
+    /**
+     * @template B
+     * @template D
+     * @param \Closure(A1):B $f
+     * @param \Closure(C1):D $g
+     * @return Constant<B,D>
+     */
     public function bimap(\Closure $f, \Closure $g): BifunctorInstance
     {
+        /**
+         * Not sure how could this be fixed and carry the B1, apart
+         * from overriding it with a var annotation.  Instead, going
+         * to suppress the error and assume static analysis will
+         * resolve from the return annotation of the method.
+         *
+         * @phpstan-ignore return.type
+         */
         return new self(c ($f) ($this->c));
     }
 }
