4bc97c0b290ae53ede59dd538b934900e4dcc253
Theo Gotsopoulos <theogpl57@gmail.com>
2025-05-08 08:15:20 +0100

    Add tests for rl()

--- tests/FunctionCompositionTest.php
+++ tests/FunctionCompositionTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use function thgs\Functional\lr;
+use function thgs\Functional\rl;
 
 class FunctionCompositionTest extends TestCase
 {
@@ -23,6 +24,29 @@ class FunctionCompositionTest extends TestCase
             fn (int $x): int => pow($x, 5),
             fn (array $items): int => count($items),
             array_filter(...)
+        );
+
+        $this->assertTrue($composed(["one", "two", ""]));
+    }
+
+    public function testCanReverseCompose2(): void
+    {
+        $composed = rl(
+            fn (int $x): int  => (int) ($x / 2),
+            fn (int $x): bool => $x == 16,
+        );
+
+        $this->assertTrue($composed(32));
+    }
+
+    public function testCanReverseCompose5(): void
+    {
+        $composed = rl(
+            array_filter(...),
+            fn (array $items): int => count($items),
+            fn (int $x): int => pow($x, 5),
+            fn (int $x): int  => (int) ($x / 2),
+            fn (int $x): bool => $x == 16,
         );
 
         $this->assertTrue($composed(["one", "two", ""]));
