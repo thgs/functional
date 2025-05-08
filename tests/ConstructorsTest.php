871dcdfbfdea78fab783fb790731ba109972e598
Theo Gotsopoulos <theogpl57@gmail.com>
2025-05-08 08:47:26 +0100

    Add tests for Either constructors

--- tests/ConstructorsTest.php
+++ tests/ConstructorsTest.php
@@ -1,11 +1,16 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use thgs\Functional\Data\Either;
 use thgs\Functional\Data\Just;
+use thgs\Functional\Data\Left;
 use thgs\Functional\Data\Maybe;
 use thgs\Functional\Data\Nothing;
+use thgs\Functional\Data\Right;
 use function thgs\Functional\just;
+use function thgs\Functional\left;
 use function thgs\Functional\nothing;
+use function thgs\Functional\right;
 
 class ConstructorsTest extends TestCase
 {
@@ -23,5 +28,21 @@ class ConstructorsTest extends TestCase
         $this->assertInstanceOf(Maybe::class, $result);
         $this->assertInstanceOf(Nothing::class, $result->getValue());
         $this->assertEquals(null, $result->getValue()->getValue());
+    }
+
+    public function testCanConstructRight(): void
+    {
+        $result = right(5);
+        $this->assertInstanceOf(Either::class, $result);
+        $this->assertInstanceOf(Right::class, $result->getValue());
+        $this->assertEquals(5, $result->getValue()->getValue());
+    }
+
+    public function testCanConstructLeft(): void
+    {
+        $result = left(5);
+        $this->assertInstanceOf(Either::class, $result);
+        $this->assertInstanceOf(Left::class, $result->getValue());
+        $this->assertEquals(5, $result->getValue()->getValue());
     }
 }
